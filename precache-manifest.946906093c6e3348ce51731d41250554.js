669019cef9213757940793f0834bfa583bde5c50
John Dahlberg <jo4415da-s@student.lu.se>
2019-07-05 23:36:37 +0200

    Updates

---
+++ precache-manifest.946906093c6e3348ce51731d41250554.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "38077599d4262a604388b28f22b9c9ef",
+    "url": "/grabb-generator/static/media/empty-card.38077599.png"
+  },
+  {
+    "revision": "3ac84e9ed18863d978f163618d647c4c",
+    "url": "/grabb-generator/static/media/card.3ac84e9e.jpg"
+  },
+  {
+    "revision": "f46510ea8d0d63688f16",
+    "url": "/grabb-generator/static/js/runtime~main.f46510ea.js"
+  },
+  {
+    "revision": "2ebcec5a2846c47e06b1",
+    "url": "/grabb-generator/static/js/main.2ebcec5a.chunk.js"
+  },
+  {
+    "revision": "61aa0df8dd2161858362",
+    "url": "/grabb-generator/static/js/2.61aa0df8.chunk.js"
+  },
+  {
+    "revision": "2ebcec5a2846c47e06b1",
+    "url": "/grabb-generator/static/css/main.9761640a.chunk.css"
+  },
+  {
+    "revision": "bb1ba23780676deddc4f00c960be51ef",
+    "url": "/grabb-generator/index.html"
+  }
+];
