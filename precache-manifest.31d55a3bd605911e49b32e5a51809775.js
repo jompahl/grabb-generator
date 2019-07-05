8ad4e81d3cb3097fc241683e12bf35d6dc25b38d
John Dahlberg <jo4415da-s@student.lu.se>
2019-07-05 23:51:45 +0200

    Updates

---
+++ precache-manifest.31d55a3bd605911e49b32e5a51809775.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "38077599d4262a604388b28f22b9c9ef",
+    "url": "/static/media/empty-card.38077599.png"
+  },
+  {
+    "revision": "3ac84e9ed18863d978f163618d647c4c",
+    "url": "/static/media/card.3ac84e9e.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ee5ad2fc1d928890f73d",
+    "url": "/static/js/main.ee5ad2fc.chunk.js"
+  },
+  {
+    "revision": "61aa0df8dd2161858362",
+    "url": "/static/js/2.61aa0df8.chunk.js"
+  },
+  {
+    "revision": "ee5ad2fc1d928890f73d",
+    "url": "/static/css/main.f4ea554d.chunk.css"
+  },
+  {
+    "revision": "aa9ea5bd9d37626df88c61e2785aab21",
+    "url": "/index.html"
+  }
+];
